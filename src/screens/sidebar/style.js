5b83f3835b9c6de8605913a58ca4cc313f046482
Yu-Chian-Lin <yuchian.eo06g@g2.nctu.edu.tw>
2019-06-08 02:35:26 +0800

    modified

--- src/screens/sidebar/style.js
+++ src/screens/sidebar/style.js
@@ -23,7 +23,8 @@ export default {
   text: {
     fontWeight: Platform.OS === "ios" ? "500" : "400",
     fontSize: 16,
-    marginLeft: 20
+    marginLeft: 20,
+    color: "#ffff"
   },
   badgeText: {
     fontSize: Platform.OS === "ios" ? 13 : 11,
