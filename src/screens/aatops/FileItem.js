a066a99fede39615d020bca22f3e0fba6875722a
Kai-Ting Weng <s104062204@m104.nthu.edu.tw>
2019-06-09 20:10:49 +0800

    function without rename

--- src/screens/aatops/FileItem.js
+++ src/screens/aatops/FileItem.js
@@ -19,7 +19,7 @@ export default class FileItem extends React.Component {
     static propTypes = {
         filename: PropTypes.string.isRequired,
         callPlay: PropTypes.func.isRequired,
-        deleteSelf: PropTypes.func.isRequired
+        deleteSelf: PropTypes.func.isRequired,
     };
 
     constructor(props) {
@@ -31,7 +31,7 @@ export default class FileItem extends React.Component {
     chooseFile() {
         this.props.callPlay(this.props.filename);
     }
-
+    
     handleDelete() {
         this.props.deleteSelf(this.props.filename);
     }
@@ -42,14 +42,13 @@ export default class FileItem extends React.Component {
                 <TouchableOpacity 
                     style={styles.playButton}
                     onPress={this.chooseFile}>
-                    <Text style={styles.fileInfo}>{ this.props.filename }</Text>
+                    <Text style={styles.fileInfo}>{this.props.filename}</Text>
                 </TouchableOpacity>
                 <Right style={{flex:4}}>
                     <View style={{flexDirection: 'row'}} >
                         <Button transparent  style={{marginHorizontal:10}}>
                             <Image style={{width:20, height:20}}  source={require("../recordermode/assets/Play/rename.png")}/>
                         </Button>
-
                         <Button transparent style={{smarginHorizontal:10,marginRight:10,marginLeft:10}}  onPress={this.handleDelete}>
                             <Image style={{width:20, height:20}}  source={require("../recordermode/assets/Play/delete.png")}/>
                         </Button>
